5ffdefc8316828505547c29d8827aa8ffa4147f8
bharathguntreddi3 <guntreddibharath@gmail.com>
2023-06-12 20:17:06 +0530

    Calculator_0816_12062023

---
+++ Task1/javascript/script.js
@@ -0,0 +1,13 @@
+function appendValue(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+    var display = document.getElementById("display").value;
+    var result = eval(display);
+    document.getElementById("display").value = result;
+}
